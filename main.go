ffe31c78e181309c60e70daa4238f5c4b51c7535
Juan Valencia <juan7732@gmail.com>
2025-09-21 21:08:16 -0500

    adding docs

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
+// This package is part of Jon Bodner's *Learning Go* chapter 10 exercise
+// The purpose is to expose an Add method that adds together two integers.
 package learningGoEx10
 
+// a: integer
+// b: integer
+// returns the sum of a and b
 func Add(a,b int) int{
 	return a + b
 }
